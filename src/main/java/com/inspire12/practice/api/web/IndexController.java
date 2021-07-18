8a19f2945e23ecbebb2c9fd60fea757883b2be64
inspire12 <ox4443@naver.com>
2021-07-19 04:30:52 +0900

    posts save api

--- src/main/java/com/inspire12/practice/api/web/IndexController.java
+++ src/main/java/com/inspire12/practice/api/web/IndexController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -30,8 +31,17 @@ public class IndexController {
         }
         return "index";
     }
-    @GetMapping("/test")
-    public String index() {
-        return "test";
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
