7ef06c90d58202bf15977e563cf3f617d5fcd64a
inspire12 <ox4443@naver.com>
2022-03-27 06:54:10 +0900

    develop - master slave routing datasource by transactional

--- src/main/java/com/inspire12/practice/api/config/datasource/ApiDataSource.java
+++ src/main/java/com/inspire12/practice/api/config/datasource/ApiDataSource.java
@@ -3,46 +3,76 @@ package com.inspire12.practice.api.config.datasource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import java.util.HashMap;
+import java.util.Map;
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 
 //https://www.baeldung.com/spring-data-jpa-multiple-databases
-
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
-    basePackages = "com.inspire12.practice.api.domain.user",
+    basePackages = "com.inspire12.practice.api.domain",
     entityManagerFactoryRef = "entityManagerFactory",
     transactionManagerRef = ApiDataSource.TX_MANAGER
 )
 public class ApiDataSource {
     public final static String TX_MANAGER ="transactionManager";
 
+    public static final String MASTER_DATASOURCE = "masterDataSource";
+    public static final String SLAVE_DATASOURCE = "slaveDataSource";
+
     @Bean
     @Primary
+    @DependsOn({MASTER_DATASOURCE, SLAVE_DATASOURCE})
+    public DataSource routingDataSource(
+            @Qualifier(MASTER_DATASOURCE) DataSource masterDataSource,
+            @Qualifier(SLAVE_DATASOURCE) DataSource slaveDataSource) {
+
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+
+        Map<Object, Object> datasourceMap = new HashMap<>() {
+            {
+                put("master", masterDataSource);
+                put("slave", slaveDataSource);
+            }
+        };
+
+        routingDataSource.setTargetDataSources(datasourceMap);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean
+    @DependsOn("routingDataSource")
+    public LazyConnectionDataSourceProxy dataSource(DataSource routingDataSource){
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+    @Bean(MASTER_DATASOURCE)
     @ConfigurationProperties(prefix = "datasource.primary")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean
-    @Primary
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 
         LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
@@ -59,10 +89,39 @@ public class ApiDataSource {
     }
 
     @Bean
-    @Primary
     PlatformTransactionManager transactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
         return transactionManager;
+    }
+
+    @Bean(SLAVE_DATASOURCE)
+    @ConfigurationProperties(prefix = "datasource.slave")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean slaveEntityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        DataSource dataSource = dataSource();
+        entityManager.setDataSource(slaveDataSource());
+        entityManager.setPackagesToScan(new String[]{"com.inspire12.practice.api.domain"});
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setJpaPropertyMap(new HashMap<String, String>() {{
+            put("hibernate.hbm2ddl.auto", "create");
+            put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+            put("hibernate.id.new_generator_mappings", "false");
+        }});
+        return entityManager;
+    }
+
+    public static class RoutingDataSource extends AbstractRoutingDataSource {
+        @Override
+        protected Object determineCurrentLookupKey() { // (1)
+            return (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) ? "slave" : "master"; //(2)
+        }
     }
 }
