49532d8268ac283a98dbdc062fcabf68470df28a
inspire12 <inspire12@ncsoft.com>
2022-06-02 17:06:53 +0900

    fix

--- src/main/java/com/inspire12/practice/api/config/auth/SecurityConfig.java
+++ src/main/java/com/inspire12/practice/api/config/auth/SecurityConfig.java
@@ -14,17 +14,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .headers().frameOptions().disable()
+            .csrf().disable() //
+            .headers().frameOptions().disable() //
+            .and()
+                .formLogin().disable()
+                .logout()
+                .logoutSuccessUrl("/")
             .and()
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**")
                     .permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
-            .and()
-                .logout()
-                    .logoutSuccessUrl("/")
             .and()
                 .oauth2Login()
                     .userInfoEndpoint()
