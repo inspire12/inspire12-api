615a9659d8d0edc2e12dc8a9ce35bbb9a695d77a
inspire12 <ox4443@naver.com>
2022-10-18 02:36:53 +0900

    test github action

--- script/bake_image.sh
+++ script/bake_image.sh
@@ -1,3 +1,4 @@
 ./mvnw clean package -DskipTests=true
 ./mvnw package -DskipTests=true
 sudo docker build --tag inspire12-api:0.0.1-SNAPSHOT .
+sudo docker tag inspire12-api:0.0.1-SNAPSHOT inspire12-api:latest
