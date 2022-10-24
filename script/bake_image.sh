a55d775b9ec5dbd5aa43db3929fe9c4f2ce1fef9
inspire12 <ox4443@naver.com>
2022-10-25 03:45:04 +0900

    build script

--- script/bake_image.sh
+++ script/bake_image.sh
@@ -1,3 +1,6 @@
+mv ../application-ouath.yml ./src/main/resources/application-ouath.yml
+mv ../application-real-db.yml ./src/main/resources/application-real-db.yml
+
 ./mvnw clean package -DskipTests=true
 ./mvnw package -DskipTests=true
 sudo docker build --tag inspire12-api:0.0.3-SNAPSHOT .
