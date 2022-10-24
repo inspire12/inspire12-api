712a35bf831235d6597f4663d7b0383c8441935c
inspire12 <ox4443@naver.com>
2022-10-25 03:46:55 +0900

    build script

--- script/bake_image.sh
+++ script/bake_image.sh
@@ -3,5 +3,5 @@ mv ../application-real-db.yml ./src/main/resources/application-real-db.yml
 
 ./mvnw clean package -DskipTests=true
 ./mvnw package -DskipTests=true
-sudo docker build --tag inspire12-api:0.0.3-SNAPSHOT .
-sudo docker tag inspire12-api:0.0.3-SNAPSHOT inspire12-api:latest
+sudo docker build --tag inspire12/inspire12-api:0.0.3-SNAPSHOT .
+sudo docker tag inspire12/inspire12-api:0.0.3-SNAPSHOT inspire12/inspire12-api:latest
