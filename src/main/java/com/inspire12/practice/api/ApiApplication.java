5edae338c9b24ea145e38d618fd74d427f522b01
inspire12 <inspire12@ncsoft.com>
2022-11-21 21:34:49 +0900

    java new feature

--- src/main/java/com/inspire12/practice/api/ApiApplication.java
+++ src/main/java/com/inspire12/practice/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.inspire12.practice.api;
 
+import com.inspire12.practice.api.config.measure.TimeChecker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class ApiApplication {
 
+    @TimeChecker // 부팅
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
---
+++ src/test/java/com/inspire12/practice/api/mq/MqTest.java
@@ -0,0 +1,14 @@
+package com.inspire12.practice.api.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MqTest {
+
+    @Test
+    void test() {
+
+    }
+
+}
