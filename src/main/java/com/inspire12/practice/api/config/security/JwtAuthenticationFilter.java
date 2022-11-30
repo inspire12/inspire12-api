7a830e4c85385bd8abe93d7523626493c9d784ce
inspire12 <inspire12-macbook@inspire12-MBB8CF.korea.ncsoft.corp>
2022-11-30 15:47:19 +0900

    develop - 서비스 버전 업

--- src/main/java/com/inspire12/practice/api/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/inspire12/practice/api/config/security/JwtAuthenticationFilter.java
@@ -1,16 +1,14 @@
 package com.inspire12.practice.api.config.security;
 
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import java.io.IOException;
 
 @Slf4j
 @Component
@@ -18,7 +16,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        log.info(servletRequest.getScheme());
+//        log.info(servletRequest.getScheme());
 //        Authentication authentication = new UsernamePasswordAuthenticationToken(claims.getSubject(), "", grantedAuthority);
 //        SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(servletRequest, servletResponse);
--- src/main/java/com/inspire12/practice/api/config/security/SecurityConfig.java
+++ src/main/java/com/inspire12/practice/api/config/security/SecurityConfig.java
@@ -2,47 +2,50 @@ package com.inspire12.practice.api.config.security;
 
 import com.inspire12.practice.api.domain.user.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() //
-            .headers().frameOptions().disable() //
+                .csrf().disable() //
+                .headers().frameOptions().disable() //
 //            .and()
 //                .sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-            .and()
+                .and()
                 .formLogin().disable()
                 .logout()
                 .logoutSuccessUrl("/")
-            .and()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/actuator/**")
-                    .permitAll()
+                .permitAll()
                 .antMatchers("/api/v1/**").permitAll()
                 .anyRequest().authenticated()
 //            .and()
 //                .exceptionHandling()
 //                .authenticationEntryPoint(customAuthenticationEntryPoint)
 //                .accessDeniedHandler(customAccessDeniedHandler)
-            .and()
+                .and()
                 .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login()
-                    .userInfoEndpoint()
-                        .userService(customOAuth2UserService);
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+        return http.build();
     }
 }
 
